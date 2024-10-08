4daf5e9940e2b494c0857a4e69e683d4511e0317
Schr0Sm1ley <radiantremel444@gmail.com>
2024-10-08 21:53:12 +0600

    Completed almost all the basic functionality

--- script/categories.js
+++ script/categories.js
@@ -9,12 +9,14 @@ const loadCategories = async () => {
 
 const displayCategories = (categories) => {
   const categoryContainer = document.getElementById("categoryContainer");
-  categories.forEach((category) => {
+  categories.forEach((item) => {
     const categoryDiv = document.createElement("div");
+    let category = `${item.category}`;
+    category = category.toLowerCase();
     const content = `
-                <button class="category${category.id} flex items-center gap-2 border-[1px] border-gray-300 rounded-lg px-10 py-4">
-                    <img src="${category.category_icon}" alt="" class="w-10 h-10">
-                    <h1 class="font-bold text-2xl">${category.category}</h1>
+                <button onclick="loadCategoryPets('${category}')" id="category-${category}" class="category-btn flex items-center gap-2 border-[1px] border-gray-300 rounded-lg px-10 py-4">
+                    <img src="${item.category_icon}" alt="" class="w-10 h-10">
+                    <h1 class="font-bold text-2xl">${item.category}</h1>
                 </button>
             `;
     categoryDiv.innerHTML = content;
--- script/pets.js
+++ script/pets.js
@@ -1,3 +1,7 @@
+// Sorting Tracks variable
+
+let isAscending = true;
+
 const loadPets = async () => {
   const url = "https://openapi.programming-hero.com/api/peddy/pets";
   const res = await fetch(url);
@@ -6,13 +10,29 @@ const loadPets = async () => {
 };
 
 const displayPets = (pets) => {
-  console.log(pets);
   const petContainer = document.getElementById("petContainer");
+  petContainer.innerHTML = "";
+  if (pets.length == 0) {
+    petContainer.classList.remove("grid");
+    const noPet = document.createElement("div");
+    noPet.classList = "bg-[#13131308] p-10 rounded-lg text-center";
+    noPet.innerHTML = `
+            <div class = "mb-3">
+              <img src="/Project/b10a6-pet-adoption-Schr0Smi1ey/images/no-pet.webp" alt="" class="mx-auto">
+            </div>
+            <h1 class="text-center font-bold text-3xl mb-3">No Information Available</h1>
+            <p class="text-center text-base text-[#131313B3]">It is a long established fact that a reader will be distracted by the readable content of a page when looking at 
+            its layout. The point of using Lorem Ipsum is that it has a.</p>
+        `;
+    petContainer.appendChild(noPet);
+    return;
+  }
 
+  petContainer.classList.add("grid");
   pets.forEach((pet) => {
     const newPet = document.createElement("div");
     const content = `
-            <div class="card w-fit shadow-xl">
+            <div id="pet-${pet.petId}" class="card w-fit shadow-xl">
                 <figure>
                 <img
                     src="${pet.image}"
@@ -46,16 +66,29 @@ const displayPets = (pets) => {
                     </div>
                     <div class="flex items-center gap-3">
                         <img src="/Project/b10a6-pet-adoption-Schr0Smi1ey/images/price.png" alt="" class="w-6 h-6">
-                        <h2 class="font-normal text-xl text-[#131313B3]">Price: <span class="text-lg"> $${
-                          pet.price ? pet.price : "Not Available"
-                        }</span></h2>
+                        <h2 class="font-normal text-xl text-[#131313B3]">Price: <span 
+                          class="text-lg price"> $${
+                            pet.price ? pet.price : "Not Available"
+                          }</span></h2>
                     </div>
                 </div>
                 <div class="divider my-1"></div>
                 <div class="flex justify-between items-center">
-                    <button class="px-3 py-1 rounded-lg border-[1px] border-gray-300"><img src="/Project/b10a6-pet-adoption-Schr0Smi1ey/images/love.png" class="h-5 w-5"></button>
-                    <button class="px-3 py-1 rounded-lg text-btnPrimary font-bold border-[1px] border-gray-300">Adopt</button>
-                    <button class="px-3 py-1 rounded-lg text-btnPrimary font-bold border-[1px] border-gray-300">Details</button>
+                    <button id="love-btn-${pet.petId}" onclick="loveEffect('${
+      pet.image
+    }', ${
+      pet.petId
+    })" class="px-3 py-1 rounded-lg border-[1px] border-gray-300">
+                      <img src="/Project/b10a6-pet-adoption-Schr0Smi1ey/images/love.svg" class="m-1 h-4 w-4">
+                    </button>
+                    <button id="adopt-${
+                      pet.petId
+                    }" onclick="displayAdoptDetails(${
+      pet.petId
+    })" class="px-3 py-1 rounded-lg text-btnPrimary font-bold border-[1px] border-gray-300">Adopt</button>
+                    <button id="details-${pet.petId}" onclick="loadDetails(${
+      pet.petId
+    })" class="px-3 py-1 rounded-lg text-btnPrimary font-bold border-[1px] border-gray-300">Details</button>
                 </div>
             </div>
         `;
@@ -65,5 +98,197 @@ const displayPets = (pets) => {
   });
 };
 
+const categoryBtnStyle = (category) => {
+  const categoryBtn = document.getElementById(`category-${category}`);
+  const allCategoryBtns = document.querySelectorAll(".category-btn");
+  allCategoryBtns.forEach((btn) => {
+    btn.classList.remove("bg-[#1c8a927a]");
+    btn.classList.remove("rounded-full");
+  });
+  categoryBtn.classList.add("bg-[#1c8a927a]");
+  categoryBtn.classList.add("rounded-full");
+};
+
+const loadCategoryPets = async (category) => {
+  const url = `https://openapi.programming-hero.com/api/peddy/category/${category}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  categoryBtnStyle(category);
+  displayPets(data.data);
+};
+
+const loadSortPets = () => {
+  const petContainer = document.getElementById("petContainer");
+  let pet = Array.from(petContainer.children);
+  if (pet.length == 0) return;
+  pet.sort((a, b) => {
+    let priceA = parseFloat(
+      a.querySelector(".price").innerText.replace("$", "")
+    );
+    let priceB = parseFloat(
+      b.querySelector(".price").innerText.replace("$", "")
+    );
+    isNaN(priceA) ? (priceA = 0) : priceA;
+    isNaN(priceB) ? (priceB = 0) : priceB;
+    return isAscending ? priceA - priceB : priceB - priceA;
+  });
+  petContainer.innerHTML = "";
+  pet.forEach((item) => {
+    petContainer.appendChild(item);
+  });
+};
+
+const sortByPrice = () => {
+  isAscending = !isAscending;
+  loadSortPets();
+};
+
+// Love Button
+const loveEffect = (image, id) => {
+  const loveBtn = document.getElementById(`love-btn-${id}`);
+  const lovedPet = document.getElementById("loved-pet");
+
+  loveBtn.classList.toggle("active");
+  if (loveBtn.classList.contains("active")) {
+    loveBtn.classList.toggle("bg-red-500");
+    loveBtn.classList.toggle("text-white");
+
+    const newLovedPet = document.createElement("div");
+    newLovedPet.id = `loved-pet-${id}`;
+    const content = `
+      <div class="flex items-center gap-3 ">
+        <img src=${image} alt="" class="rounded-lg">
+      </div>
+    `;
+    newLovedPet.innerHTML = content;
+    lovedPet.appendChild(newLovedPet);
+  } else {
+    loveBtn.classList.toggle("bg-red-500");
+    loveBtn.classList.toggle("text-white");
+    const lovedPet = document.getElementById(`loved-pet-${id}`);
+    lovedPet.remove();
+  }
+};
+
+const loadDetails = async (id) => {
+  console.log(id);
+  const uri = `https://openapi.programming-hero.com/api/peddy/pet/${id}`;
+  const res = await fetch(uri);
+  const data = await res.json();
+  displayDetails(data.petData, id);
+};
+
+const displayDetails = (petData, id) => {
+  const detailContainer = document.getElementById("detailContainer");
+  const content = `
+      <div class ="mb-3">
+        <img src=${petData.image} alt="" class="w-full rounded-lg"/>
+      </div>
+      <div class="">
+        <h1 class="font-bold text-3xl mb-2">${petData.pet_name}</h1>
+        <div class="">
+          <div class="flex items-center gap-3">
+              <img src="/Project/b10a6-pet-adoption-Schr0Smi1ey/images/breed.png" alt="" class="w-6 h-6">
+              <h2 class="font-normal text-xl text-[#131313B3]">Breed: <span class="text-lg"> ${
+                petData.breed ? petData.breed : "Not Available"
+              }</span>
+              </h2>
+          </div>
+          <div class="flex items-center gap-3">
+              <img src="/Project/b10a6-pet-adoption-Schr0Smi1ey/images/birth.png" alt="" class="w-6 h-6">
+              <h2 class="font-normal text-xl text-[#131313B3]">Birth: <span class="text-lg"> ${
+                petData.date_of_birth ? petData.date_of_birth : "Not Available"
+              }</span></h2>
+          </div>
+          <div class="flex items-center gap-3">
+              <img src="/Project/b10a6-pet-adoption-Schr0Smi1ey/images/gender.png" alt="" class="w-6 h-6">
+              <h2 class="font-normal text-xl text-[#131313B3]" >Gender: <span class="text-lg"> ${
+                petData.gender ? petData.gender : "Not Available"
+              }</span></h2>
+          </div>
+          <div class="flex items-center gap-3">
+              <img src="/Project/b10a6-pet-adoption-Schr0Smi1ey/images/price.png" alt="" class="w-6 h-6">
+              <h2 class="font-normal text-xl text-[#131313B3]">Price: <span
+                class="text-lg price"> $${
+                  petData.price ? petData.price : "Not Available"
+                }</span></h2>
+          </div>
+          <div class="flex items-center gap-3">
+              <img src="/Project/b10a6-pet-adoption-Schr0Smi1ey/images/vaccinated.png" alt="" class="w-6 h-6">
+              <h2 class="font-normal text-xl text-[#131313B3]">Vaccinated Status: <span
+                class="text-lg price"> ${
+                  petData.vaccinated_status
+                    ? petData.vaccinated_status
+                    : "Not Available"
+                }</span></h2>
+          </div>
+      </div>
+      <div class="divider my-1"></div>
+      <div>
+          <h1 class="font-semibold text-base mb-2">Details Information</h1>
+          <p>${petData.pet_details}</p>
+      </div>
+    `;
+  detailContainer.innerHTML = content;
+  document.getElementById("details").showModal();
+};
+
+const displayAdoptDetails = (id) => {
+  const adoptDetails = document.getElementById("adoptDetails");
+  let counter = 3; // Initialize the counter at 3
+
+  const content = `
+      <div class="text-center mb-3">
+        <img src="/Project/b10a6-pet-adoption-Schr0Smi1ey/images/hand-shake.png" alt="" class="w-24 h-24 rounded-lg mx-auto">
+        <h1 class="font-[900] text-5xl mb-4">Congrats</h1>
+        <h2 class="font-medium text-lg text-gray-400 mb-4">Adoption process is starting for your pet</h2>
+        <span class="countdown font-mono text-6xl">
+          <span class="font-[900]" style="--value:${counter};">${counter}</span>
+        </span>
+      </div>
+    `;
+  adoptDetails.innerHTML = content;
+
+  document.getElementById("adopt").showModal();
+
+  const interval = setInterval(() => {
+    counter--;
+    if (counter >= 0) {
+      adoptDetails
+        .querySelector(".countdown span")
+        .style.setProperty("--value", counter);
+      adoptDetails.querySelector(".countdown span").innerText = counter;
+    }
+
+    if (counter <= 0) {
+      document.getElementById("cancelButton").click();
+      clearInterval(interval);
+    }
+  }, 1000);
+  document.getElementById(`adopt-${id}`).classList.add("btn-disabled");
+  document.getElementById(`adopt-${id}`).classList.add("text-opacity-40");
+  document.getElementById(`adopt-${id}`).classList.add("cursor-not-allowed");
+  document.getElementById(`adopt-${id}`).disabled.innerText = "Adopted";
+};
+
 loadPets();
 displayPets();
+
+/**
+ * {
+  "status": true,
+  "message": "successfully fetched pet data using id 1",
+  "petData": {
+    "petId": 1,
+    "breed": "Golden Retriever",
+    "category": "Dog",
+    "date_of_birth": "2023-01-15",
+    "price": 1200,
+    "image": "https://i.ibb.co.com/p0w744T/pet-1.jpg",
+    "gender": "Male",
+    "pet_details": "This friendly male Golden Retriever is energetic and loyal, making him a perfect companion for families. Born on January 15, 2023, he enjoys playing outdoors and is especially great with children. Fully vaccinated, he's ready to join your family and bring endless joy. Priced at $1200, he offers love, loyalty, and a lively spirit for those seeking a playful yet gentle dog.",
+    "vaccinated_status": "Fully",
+    "pet_name": "Sunny"
+  }
+}
+ */
